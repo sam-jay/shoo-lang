0b15685dfbc2adab4bde8f4a16215d51d8937162
Crystal Ren <crystalren97@gmail.com>
2018-10-14 17:39:47 -0400

    added test for function var in struct

--- test/TParser.ml
+++ test/TParser.ml
@@ -398,6 +398,27 @@ let string_tests =
     "Should accept string literal" >::string_lit_test;
   ]
 
+(* first-class function as variables *)
+let function_variable test_ctxt = assert_equal 
+  [FDecl("temp",[(Int, "y")],Int,[Return(Binop(Id("y"),Add,IntLit(5)))]);
+  StructDef("Baz",
+    [(Func, "f", Some(Id("temp"))); (Int,"field2", None)])
+]
+
+(parse "function temp(int y) int {                  
+        return y+5;
+}
+ struct Baz { // has function members
+    func f = temp;
+    int field2;
+}")
+
+let func_tests  =
+  "Functions" >:::
+  [
+    "First-class functions as variables" >::function_variable;
+  ]
+
 let tests =
   "Parser" >:::
   [
@@ -415,4 +436,5 @@ let tests =
     array_tests;
     new_tests;
     string_tests;
+    func_tests;
   ]
