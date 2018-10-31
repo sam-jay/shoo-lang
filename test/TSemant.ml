f92bf49839e0ed978bc97ee72f418c83432ffca7
TCXX <tiancai_xinxin@126.com>
2018-10-31 13:45:00 -0400

    Added tests for variable assignment

--- test/TSemant.ml
+++ test/TSemant.ml
@@ -7,15 +7,35 @@ let check input =
   let _ = Semant.check_program program in
   ""
 
+(* Tests that should pass *)
+
 let asn_int_int test_ctxt = assert_equal "" (check "int x; x = 5;")
+let asn_bool_bool test_ctxt = assert_equal "" (check "bool b; b = false;")
+let asn_str_str test_ctxt = assert_equal "" (check "string s; s = \"abs\";")
+
+(* Tests that should fail *)
 
 let asn_int_str test_ctxt =
   let f = fun () -> check "int x; x = \"foo\";" in
   assert_raises (Semant.Invalid_assignment "type mismatch in assignment") f
 
+let asn_int_bool test_ctxt =
+  let f = fun () -> check "int i; i = true;" in
+  assert_raises (Semant.Invalid_assignment "type mismatch in assignment") f
+
+let asn_bool_int test_ctxt =
+  let f = fun () -> check "bool b; b = 48;" in
+  assert_raises (Semant.Invalid_assignment "type mismatch in assignment") f
+
+
 let tests =
   "Semantic checker" >:::
   [
     "Int to int assignment" >:: asn_int_int;
-    "String to int assignment" >:: asn_int_str
+    (* "Bool to bool assignment" >:: asn_bool_bool; *)
+    "String to string assignment" >:: asn_str_str;
+    
+    "String to int assignment" >:: asn_int_str;
+    "Bool to int assignment" >:: asn_int_bool;
+    "Int to bool assignment" >:: asn_bool_int
   ]
