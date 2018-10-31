75a079abba87677fe80afe3bbfb59b92b9296af8
TCXX <tiancai_xinxin@126.com>
2018-10-31 14:04:10 -0400

    Added comments

--- test/TSemant.ml
+++ test/TSemant.ml
@@ -7,13 +7,13 @@ let check input =
   let _ = Semant.check_program program in
   ""
 
-(* Tests that should pass *)
+(* Variable Assignment - Pass *)
 
 let asn_int_int test_ctxt = assert_equal "" (check "int x; x = 5;")
 let asn_bool_bool test_ctxt = assert_equal "" (check "bool b; b = false;")
 let asn_str_str test_ctxt = assert_equal "" (check "string s; s = \"abs\";")
 
-(* Tests that should fail *)
+(* Variable Assignment - Fail *)
 
 let asn_int_str test_ctxt =
   let f = fun () -> check "int x; x = \"foo\";" in
@@ -26,16 +26,24 @@ let asn_int_bool test_ctxt =
 let asn_bool_int test_ctxt =
   let f = fun () -> check "bool b; b = 48;" in
   assert_raises (Semant.Invalid_assignment "type mismatch in assignment") f
+  
+(* Function Declaration - Pass *)
 
+let func_dec_int test_ctxt = assert_equal "" (check "function int main() { return 0; }")
 
 let tests =
   "Semantic checker" >:::
   [
+    (* Variable Assignment *)
+    
     "Int to int assignment" >:: asn_int_int;
     "Bool to bool assignment" >:: asn_bool_bool;
     "String to string assignment" >:: asn_str_str;
     
     "String to int assignment" >:: asn_int_str;
     "Bool to int assignment" >:: asn_int_bool;
-    "Int to bool assignment" >:: asn_bool_int
+    "Int to bool assignment" >:: asn_bool_int;
+    
+    (* Function Declaration *)
+    (* "Function declaration that returns int" >:: func_dec_int; *)
   ]
