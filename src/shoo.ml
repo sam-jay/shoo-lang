b9968bb6169f235e29129feba1b7505d95d6a984
Alfred Aho <crystalren97@gmail.com>
2018-11-03 23:10:13 -0400

    implem -a|-s in shoo.ml

--- src/shoo.ml
+++ src/shoo.ml
@@ -2,14 +2,14 @@
    check the resulting AST and generate an SAST from it, generate LLVM IR,
    and dump the module *)
 
-type action = Ast (*| Sast*) | LLVM_IR | Compile
+type action = Ast | Sast | LLVM_IR | Compile
 
 let () =
   let action = ref Compile in
   let set_action a () = action := a in
   let speclist = [
     ("-a", Arg.Unit (set_action Ast), "Print the AST");
-    (* ("-s", Arg.Unit (set_action Sast), "Print the SAST"); *)
+    ("-s", Arg.Unit (set_action Sast), "Print the SAST");
     ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
     ("-c", Arg.Unit (set_action Compile),
       "Check and print the generated LLVM IR (default)");
@@ -21,11 +21,11 @@ let () =
   let lexbuf = Lexing.from_channel !channel in
   let ast = Parser.program Scanner.token lexbuf in  
   match !action with
-    (*Ast -> print_string (Ast.string_of_program ast)*)
+    Ast -> print_string (Ast.string_of_program ast)
   | _ -> let sast = Semant.check_program ast in
     match !action with
       Ast     -> ()
-    (*| Sast    -> print_string (Sast.string_of_sprogram sast)*)
+    | Sast    -> print_string (Sast.string_of_sprogram sast)
     | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate sast))
     | Compile -> let m = Codegen.translate sast in
       Llvm_analysis.assert_valid_module m;
