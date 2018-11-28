5b33121f651b8fa1f16f794a5725af16a8e14727
Alfred Aho <crystalren97@gmail.com>
2018-11-28 17:06:45 -0500

    added some builtins

--- src/semant.ml
+++ src/semant.ml
@@ -429,6 +429,10 @@ let builtins = [
   ("str_of_bool", SFunc({ sparam_typs = [SBool]; sreturn_typ = SString }));
   ("string_concat", SFunc({ sparam_typs = [SString; SString]; sreturn_typ = SString })); 
   ("string_equals", SFunc({ sparam_typs = [SString; SString]; sreturn_typ = SInt })); 
+  ("str_of_float", SFunc({ sparam_typs = [SFloat]; sreturn_typ = SString })); 
+  ("int_of_float", SFunc({ sparam_typs = [SFloat]; sreturn_typ = SInt })); 
+  ("float_of_int", SFunc({ sparam_typs = [SInt]; sreturn_typ = SFloat })); 
+  ("scan_line", SFunc({ sparam_typs = [SInt]; sreturn_typ = SString })); 
 ]
 
 let def_ctxt =
